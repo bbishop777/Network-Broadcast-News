df3b0ae2c83e6b9a0e69315e918161c7838d63d0
BBishop <brad.bishop68@gmail.com>
2016-01-09 21:46:15 -1000

    Adds username registration that partly works

---
+++ name.js
@@ -0,0 +1,3 @@
+var name= [];
+
+module.exports = name;
--- server.js
+++ server.js
@@ -1,21 +1,57 @@
 var net = require('net');
+var fs = require('fs');
+var names = require('./name.js');
 var clients = [];
 
+
 var Server = net.createServer(function (client) {
+  client.setEncoding('utf-8');
   clients.push(client);
+
+  client.write('Welcome! Please type in \'Username:\' followed by a space then your username\n' +
+   '(if not registered, type \'Username:\' then space, and enter a new username):\n' + '==>:');
+
+
+  //clients[clients.length-1].username = null;
   console.log(clients[clients.length-1].remotePort);
 
   client.on('data', function (data) {
-    console.log(data);
-    client.write(data);
+    if(data.search(/username:/i) >= 0) {
+      return registration(client, data);
+    } else { //could do client.destroy here if they don't put in username.  If they do
+    //then have the registration function call another function with the lines below
+    //for posting
+      for (var i = 0; i < clients.length; i++) {
+        if(clients[i] !== client) {
+          // console.log(data);
+          clients[i].write(data);
+        }
+      }
+    }
   });
+
+  function registration(client, data) {
+    var usrNameSplit = data.split(' ');
+    client.username = usrNameSplit[1];
+    client.write('Welcome ' + client.username);
+    if(names.indexOf(client.username) >= 0) {
+      client.write(client.username + ' I see you are already registered! Welcome back.\n');
+    } else {
+      names.push(client.username);
+      names = (JSON.stringify(names));
+      fs.writeFileSync('./name.js', 'var name = ' + names + ';\n' + 'module.exports = name;');
+    }
+
+  }
+
+
 
   client.on('end', function() {
     console.log('Someone disconnected');
     for (var i = 0; i < clients.length; i++) {
       if (clients[i] === client) {
         console.log(clients.length);
-        console.log('Removed client', client.remotePort);
+        console.log('Removing client', client.remotePort);
         clients.splice(i, 1);
         console.log(clients.length);
 
@@ -23,12 +59,22 @@ var Server = net.createServer(function (client) {
     }
   });
 
+//   function registration(client) {
+//     client.write('Enter your username if you have registered. If not registered hit enter');
+//     client.on('data', function(data) {
+//       console.log('At the registration', data);
+//     });
 
-  client.setEncoding('utf-8');
-  client.write('Welcome! Please type message and hit return\n');
+//     client.write('Please register by entering a username:\n');
+//   }
+
 });
 
-Server.listen({port: 6969, host: 'localhost'}, function (){
+Server.listen(6969, 'localhost', function (){
   console.log('Server is running');
 
 });
+// Server.listen(6969, '0.0.0.0', function (){
+//   console.log('Server is running');
+
+// });
