86ef661d6e5c31b49e8359013040236bef9a3e99
BBishop <brad.bishop68@gmail.com>
2016-01-13 10:20:22 -1000

    Adds limit timer that disconnects client if they post more than 10 times in 2 seconds

--- name.js
+++ name.js
@@ -1,2 +1,2 @@
-var name = ["Jeff","Pete","Joey","pete","Brad","Geee","Joe","Rhonda","peach","pooh","Pooh","Vic","Beach","Peach","Jo","Blow","hard"];
+var name = ["Jeff","Pete","Joey","pete","Brad","Geee","Joe","Rhonda","peach","pooh","Pooh","Vic","Beach","Peach","Jo","Blow","hard","George","Hacker","Bad","Troller"];
 module.exports = name;
--- server.js
+++ server.js
@@ -3,25 +3,30 @@ var fs = require('fs');
 var names = require('./name.js');
 var clients = [];
 
-
-
+//This controls the behavior for when Server-side admin types anything
+//and presses enter
+//
+//the first condition looks to see if Admin types kick and a username and
+//initiates a process to remove the client's connection
 process.stdin.on('data', function(data) {
-    if (data.search(/kick/i) === 0) {
-      var whosOut = data.split(' ');
-      var bootThem = whosOut[1].trim();
-      var kStatus = kick(bootThem);
-      if (kStatus === true) {
-        return;
-      }
+  if (data.search(/kick/i) === 0) {
+    var whosOut = data.split(' ');
+    var bootThem = whosOut[1].trim();
+    var kStatus = kick(bootThem);
+    if (kStatus === true) {
+      return;
     }
-    for(var p = 0; p < clients.length; p++) {
-      if(clients[p].username !== undefined) {
-        clients[p].write('\r[ADMIN]:' + data +'me:');
-        process.stdout.write('\rServer: ');
-      }
-   }
+  }
+  //if not kicking someone...the ADMIN broadcasts to everyone
+  for(var p = 0; p < clients.length; p++) {
+    if(clients[p].username !== undefined) {
+      clients[p].write('\r[ADMIN]:' + data +'me:');
+      process.stdout.write('\rServer: ');
+    }
+ }
 });
 
+//function to disconnect the client
 function kick(bootThem) {
   console.log("Hereeeee");
   for (var j = 0; j < clients.length; j++) {
@@ -34,26 +39,72 @@ function kick(bootThem) {
   }
 }
 
+//this timer is initiated below when client first starts typing.  If they make
+//more than 10 entries in 2 seconds they are disconnected from Server
+function myTimer (limitTimer, client) {
+      if (client.count > 10) {
+        client.write('\r[ADMIN]: You have exceeded posts per second.  You are being removed from the Chatroom!\n');
+        process.stdout.write('\rServer To ' + client.username + ': You have exceeded posts per second.  You are being removed from the Chatroom!\nServer: ');
+        kick(client.username);
+        console.log('Im kicking ' + client.username);
+        stopMyTimer(limitTimer, client);
+      } else {
+        console.log(client.username + ' Ok number of posts');
+        stopMyTimer(limitTimer, client);
+      }
+    }
+//this function stops the timer which is initiated in the limitTimer.  It does
+//not stop the function...the function only starts a process.  The var limitTimer
+//references it and is passed along from function to function.  ClearInterval finds
+//the Timer process and stops it.
+function stopMyTimer(limitTimer, client) {
+  client.count = 0;
+  hasTimer = false;
+  clearInterval(limitTimer);
+}
+
+//creates instance of a server and client (when client.js is run on port 6969)
 var Server = net.createServer(function (client) {
   client.setEncoding('utf-8');
   clients.push(client);
+  client.count = 0;
+  var hasTimer = false;
 
+  //sends initial message to client
   client.write('Welcome! Please type in \'Username:\' followed by a space then your username\n' +
    '(if not registered, type \'Username:\' then space, and enter a new username\n' + '(include at least 3 different numbers and 5 letters)):\n' + '==>:');
 
-
+  //Server to its console communication
   process.stdout.write('\rServer: ' + clients[clients.length-1].remotePort + ' ' + clients.length + '\n');
 
+  //sets encoding Server side
   process.stdin.setEncoding('utf-8');
 
 
+  //This is activated everytime a client hits enter whether they type something or not
+  //
   client.on('data', function (data) {
+
+    //checks to see if hasTimer is set to false...if it is, it starts a new one on the client.
+    //This prevents a new timer being started with each post from the same client.  A new timer
+    //is only started on the same client after 2 seconds with the post limit not exceeded
+    if(!hasTimer) {
+      var limitTimer = setInterval(function() {
+        myTimer(limitTimer, client);
+      }, 2000);
+    }
+
+    client.count += 1;
+
+
+
     if(data.search(/username:/i) === 0) {
       return registration(client, data);
     } else if (client.username === undefined) {
       client.status = 'bad';
       incorrectUsername(client);
     } else if (client.username.length > 0) {
+
       return post(client, data);
     }
   });
@@ -130,12 +181,13 @@ var Server = net.createServer(function (client) {
   }
 
   client.on('end', function() {
+
     process.stdout.write('\rServer: ' + client.username + ' disconnected\nServer: ');
       console.log('Heeeereee?', clients.length);
     for (var i = 0; i < clients.length; i++) {
       if ((client.status !== 'bad') && (clients[i] !== client)) {
         clients[i].write('\r' + client.username +' has left the building!\nme: ');
-        process.stdout.write('\rServer: To ' + clients[i].username + ': ' + client.username + 'has left the building!\nServer: ');
+        process.stdout.write('\rServer: To ' + clients[i].username + ': ' + client.username + ' has left the building!\nServer: ');
       }
     }
 
